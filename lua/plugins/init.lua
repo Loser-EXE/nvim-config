e4528a5093699f814040062edbda293eb1b83f27
LoserEXE <christopherlemus@protonmail.com>
2024-05-17 14:33:50 -0500

    Updated Tabs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,34 +1,34 @@
 return {
-	{"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		config = true},
-	{"numToStr/Comment.nvim",
-		lazy = false,
-		opts = {}
-	},
-	{"nvim-tree/nvim-web-devicons",
-		config = function()
-			require("nvim-web-devicons")
-		end
-	},
-	{"vim-airline/vim-airline"},
-	{"nvim-lua/plenary.nvim"},
-	{"MunifTanjim/nui.nvim"},
-	{"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
+    {"ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = true},
+    {"numToStr/Comment.nvim",
+        lazy = false,
+        opts = {}
+    },
+    {"nvim-tree/nvim-web-devicons",
+        config = function()
+            require("nvim-web-devicons")
+        end
+    },
+    {"vim-airline/vim-airline"},
+    {"nvim-lua/plenary.nvim"},
+    {"MunifTanjim/nui.nvim"},
+    {"nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
         requires = {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons",
             "MunifTanjim/nui.nvim",
         }
-	},
+    },
     {"nvim-treesitter/nvim-treesitter",
         config = function()
             require("plugins/config/treesitter")
         end
     },
     {"L3MON4D3/LuaSnip",
-	    build = "make install_jsregexp"
+        build = "make install_jsregexp"
     },
     {"hrsh7th/cmp-buffer"},
     {"saadparwaiz1/cmp_luasnip"},
