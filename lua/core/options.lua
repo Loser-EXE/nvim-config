1769a8d48bc1c176dc3d91519135c57f1e6e4129
LoserEXE <christopherlemus@protonmail.com>
2024-05-19 18:33:36 -0500

    Folding

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -10,5 +10,9 @@ set.shiftwidth = 4
 set.expandtab = true
 set.smartindent = true
 set.autoindent = true
+-- Folds
+set.foldmethod = "indent"
+set.foldlevel = 99
+set.foldcolumn = "4" -- Has to be a string for some reason
 
 vim.cmd.colorscheme("gruvbox")
